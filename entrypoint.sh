d0cf529d6d70c78fc4a41793864cb3893c529e46
Aseem Bansal <asmbansal2@gmail.com>
2023-03-19 01:11:31 +0530

    actually do ingestion

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,7 @@
 time=$(date)
 echo "echo started at time=$time"
 
-ls -alh
-
-echo "recipe file is "
-cat "$1"
-
-echo "pip freeze is"
-pip freeze
-
 echo "installing cli specified"
 pip install --upgrade "acryl-datahub==$4"
+
+datahub ingest -c "$1"
